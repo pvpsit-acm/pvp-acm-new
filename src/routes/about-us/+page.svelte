7c2f3d7a20b41cfeaad617ce95bf1dd1d3f8bfa7
nagaumamahesh <nandi.umamahesh.2003@gmail.com>
2024-01-24 15:13:44 +0530

    AboutUs content

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Contact</h1>
-<p>New phone, who dis?</p>
+<div>
+	<div><h2>ACM Student Chapter PVPSIT</h2></div>
+
+	<div><h4>Empowering Minds, Shaping Futures</h4></div>
+	<p>
+		ACM Student Chapter at PVP Siddhartha Institute of Technology (PVPSIT) is a premier hub for
+		technology enthusiasts, learners, and innovators, we invite you to embark on a journey of
+		exploration and growth in the ever-evolving realm of computing.
+	</p>
+</div>
